c90c4201888312ea44c3e6998e242bf81a3e99fa
Cai Yang <caih.yang@gmail.com>
2019-04-19 16:23:58 -0400

    Search Bar & ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 #Ani-Manage
+
+
+
+![IMG_4138](https://user-images.githubusercontent.com/33525692/56442588-596b7180-62bf-11e9-9d2a-d5d75ef534f4.jpeg)
+![IMG_4139](https://user-images.githubusercontent.com/33525692/56442603-60927f80-62bf-11e9-8c2d-220833fc6437.jpeg)
--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Route, Link} from 'react-router-dom'
+import SearchBar from './assets/SearchBar.js'
 
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchVal: '',
+      genre: ""
+    };
+  }
+
+  searchHandle = (event) => {
+    this.setState({searchVal: event.target.value});
+  }
+
+  fetchSearched = (event) => {
+    console.log('HERLERLESOSDKF');
+  }
+
   render() {
-    return (
-      <div>
-        <h1>SUPP</h1>
-      </div>
-    );
+    const {searchVal,byGenre} = this.state;
+    return (<div>
+
+      <SearchBar
+        searchHandle={this.searchHandle}
+        fetchSearched={this.fetchSearched}
+      />
+
+    </div>);
   }
 }
 
