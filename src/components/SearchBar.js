132ec1552cf87eeaa6d2f766a8805bd8359fd2f5
Cai Yang <caih.yang@gmail.com>
2019-04-21 03:10:33 -0400

    searchBar styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -31,23 +31,25 @@ class SearchBar extends Component {
     const {fetchSearched, searchHandle,genreHandle,fetchGenre} = this.props;
     const {byGenre} = this.state;
     if (byGenre) {
-      return <Form className ="genre-form big" onSubmit={fetchGenre}>
-        <Form.Field
-          className = "genre-search"
-          control={Select}
-          options={options}
-          placeholder="Select Genre"
-          onChange={(e, { value }) => genreHandle(value)}
-        />
-        <Button
-          className="button-style"
-          type="submit"
-          icon ='search'
-        >
-        </Button>
+      return <Form className ="genre-form large" onSubmit={fetchGenre}>
+        <Form.Group>
+          <Form.Field
+            className = "genre-search large"
+            control={Select}
+            options={options}
+            placeholder="Select Genre"
+            onChange={(e, { value }) => genreHandle(value)}
+          />
+          <div className="need-space"></div>
+          <Button
+            type="submit"
+            icon ='search'
+            content="Search"
+          />
+        </Form.Group>
       </Form>
     } else {
-      return <Form className="genre-search big" onSubmit={() => fetchSearched()}>
+      return <Form className="title-search large" onSubmit={() => fetchSearched()}>
         <Input
           label="Find Anime !"
           onChange={searchHandle}
@@ -67,12 +69,12 @@ class SearchBar extends Component {
   render() {
 
     return (<div className = "search-bar-style">
-      <div>
+      <div className ="toggles">
         <Radio toggle="toggle" onClick={this.handleToggle}/>
         {
           (this.state.byGenre)
             ? <h3 className="search-style">Search by Genre !</h3>
-            : <h3 className="search-style">Search by Title</h3>
+            : <h3 className="search-style">Search by Title !</h3>
         }
       </div>
       {this.showSearchBar()}
